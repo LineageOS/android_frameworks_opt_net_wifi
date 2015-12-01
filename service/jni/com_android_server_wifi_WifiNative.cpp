2a75b82118d406bb892392919c16bb298ebcbc7b
Shun Iio <shun.xa.iio@sonymobile.com>
2015-12-01 10:11:41 +0900

    Fix potential memory leak
    Variable "buffer" falls out of scope leaking memory.
    
    Change-Id: Ia9ca5b1652507e64a6a53c3c7d668684c1ed327e

--- service/jni/com_android_server_wifi_WifiNative.cpp
+++ service/jni/com_android_server_wifi_WifiNative.cpp
@@ -1578,6 +1578,7 @@ static jobject android_net_wifi_get_driver_version(JNIEnv *env, jclass cls, jint
     ALOGD("android_net_wifi_get_driver_version = %p", handle);
 
     if (handle == 0) {
+        free(buffer);
         return NULL;
     }
 
@@ -1608,6 +1609,7 @@ static jobject android_net_wifi_get_firmware_version(JNIEnv *env, jclass cls, ji
     ALOGD("android_net_wifi_get_firmware_version = %p", handle);
 
     if (handle == 0) {
+        free(buffer);
         return NULL;
     }
 
