40c9f8da1ab5f024bd141508b19319abed6683e6
Aaron Kling <webgeek1234@gmail.com>
2017-04-07 13:21:36 -0500

    Allow activing a saved autonomous group [2/2]
    Change-Id: I1212bec0992fcafa22f055983d614990fd61ac02

--- service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
+++ service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
@@ -1962,6 +1962,8 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
                                         null);
                                 ret = mWifiNative.p2pGroupAdd(true);
                             }
+                        } else if (netId >= 0) {
+                            ret = mWifiNative.p2pGroupAdd(netId);
                         } else {
                             mWifiP2pMetrics.startConnectionEvent(
                                     P2pConnectionEvent.CONNECTION_LOCAL,
