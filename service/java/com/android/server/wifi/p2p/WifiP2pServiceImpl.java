8ded6428ca5fee236a3ad82f2e805b8890138c5e
Aaron Kling <webgeek1234@gmail.com>
2017-04-07 13:22:44 -0500

    Better track autonomous group creation
    Change-Id: I8d89a054a981973a70c5af244933b80e6def28e1

--- service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
+++ service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
@@ -199,6 +199,10 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
     // is invoked
     private boolean mAutonomousGroup;
 
+    // Even if creation of the group fails, we need to know if the interface
+    // exists
+    private boolean mAutonomousGroupCreated;
+
     // Invitation to join an existing p2p group
     private boolean mJoinExistingGroup;
 
@@ -2156,6 +2160,7 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
                 // after a client joins. For autonomous, send now
                 if (mAutonomousGroup) {
                     sendP2pConnectionChangedBroadcast();
+                    mAutonomousGroupCreated = true;
                 }
             }
 
@@ -2253,6 +2258,8 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
                     case WifiP2pManager.REMOVE_GROUP:
                         if (DBG) logd(getName() + " remove group");
                         if (mWifiNative.p2pGroupRemove(mGroup.getInterface())) {
+                            mAutonomousGroup = false;
+                            mAutonomousGroupCreated = false;
                             transitionTo(mOngoingGroupRemovalState);
                             replyToMessage(message, WifiP2pManager.REMOVE_GROUP_SUCCEEDED);
                         } else {
@@ -3039,6 +3046,8 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
         }
 
         private void handleGroupCreationFailure() {
+            mAutonomousGroup = false;
+            mAutonomousGroupCreated = false;
             resetWifiP2pInfo();
             mNetworkInfo.setDetailedState(NetworkInfo.DetailedState.FAILED, null, null);
             sendP2pConnectionChangedBroadcast();
@@ -3060,6 +3069,8 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
         }
 
         private void handleGroupRemoved() {
+            mAutonomousGroup = false;
+            mAutonomousGroupCreated = false;
             if (mGroup.isGroupOwner()) {
                 stopDhcpServer(mGroup.getInterface());
             } else {
