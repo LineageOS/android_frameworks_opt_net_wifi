957661abafb575035d3d0c32f289f6f9675c5e45
Aaron Kling <webgeek1234@gmail.com>
2017-11-07 13:55:22 -0600

    Expose isAutonomousGroupOwnerInitiated [2/2]
    Change-Id: I00bb98d6f8ef910d75df17e100a7d729f197b830

--- service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
+++ service/java/com/android/server/wifi/p2p/WifiP2pServiceImpl.java
@@ -610,6 +610,14 @@ public class WifiP2pServiceImpl extends IWifiP2pManager.Stub {
         mP2pStateMachine.sendMessage(SET_MIRACAST_MODE, mode);
     }
 
+    /** This is used to provide the information that there is an active autonomous GO and
+      * applications should call create/remove p2p interface accordingly
+      */
+    @Override
+    public boolean isAutonomousGroupOwnerInitiated() {
+        return mAutonomousGroup;
+    }
+
     @Override
     public void checkConfigureWifiDisplayPermission() {
         if (!getWfdPermission(Binder.getCallingUid())) {
