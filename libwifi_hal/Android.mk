cfeea0a4e9036a80cc651a03342a35e9740b8b67
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-01-14 13:08:27 +0100

    wifi: add optional wait for driver probe
    Some drivers take some time for probing, especially for loading firmware or depend on other services to get
    ready. Add option to block wifi_load_driver() for a specific amount of time.
    
    Based on
    https://github.com/omnirom/android_frameworks_opt_net_wifi/commit/12ecdd5450400486063de5e9caa5fe10f7459bd6
    
    Test: Reboot device and check if wifi is enabled after boot
    
    Change-Id: If3ba3318d3e22bfb90d142e95b15b9ea1c33d2c6

--- libwifi_hal/Android.mk
+++ libwifi_hal/Android.mk
@@ -60,6 +60,11 @@ ifdef WIFI_DRIVER_STATE_OFF
 wifi_hal_cflags += -DWIFI_DRIVER_STATE_OFF=\"$(WIFI_DRIVER_STATE_OFF)\"
 endif
 
+# Wifi driver probe delay in seconds
+ifdef WIFI_DRIVER_PROBE_DELAY
+wifi_hal_cflags += -DWIFI_DRIVER_PROBE_DELAY=\"$(WIFI_DRIVER_PROBE_DELAY)\"
+endif
+
 # Common code shared between the HALs.
 # ============================================================
 include $(CLEAR_VARS)
