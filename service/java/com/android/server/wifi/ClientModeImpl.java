1554887cf1ced589b9f37d0cbf18eb266795ea1f
xshu <xshu@google.com>
2018-09-24 15:02:09 -0700

    Wifi usability metrics collection
    Collects labeled list of timestamped wifi stats
    
    Data is flagged as LABEL_BAD when a wifi unusable event occurs.
    Data is flagged as LABEL_GOOD when wifi doesn't experience any unusable
    event for 100 rssi polls, which is approximately 5 minutes of screen on
    usage.
    
    To make sure the data upload is useful (ie we don't get too much of one
    type of labeled result), post processing is done to make sure the
    generated proto contains a LABEL_BAD for each LABEL_GOOD and extra
    results are discarded.
    
    Bug: 113262380
    Bug: 112599656
    Test: compile, unit tests
    Test: Connect to wifi, wait 10 seconds then
    run "adb shell dumpsys wifi | grep -A70 mWifiUsabilityStatsEntriesList" and view printout
    Change-Id: I21892190305e4667d6dec81e1d9de2f9ec2bc2ef

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -99,6 +99,7 @@ import com.android.server.wifi.hotspot2.PasspointManager;
 import com.android.server.wifi.hotspot2.WnmData;
 import com.android.server.wifi.nano.WifiMetricsProto;
 import com.android.server.wifi.nano.WifiMetricsProto.StaEvent;
+import com.android.server.wifi.nano.WifiMetricsProto.WifiUsabilityStats;
 import com.android.server.wifi.p2p.WifiP2pServiceImpl;
 import com.android.server.wifi.util.NativeUtil;
 import com.android.server.wifi.util.TelephonyUtil;
@@ -4726,12 +4727,15 @@ public class ClientModeImpl extends StateMachine {
                 case CMD_RSSI_POLL:
                     if (message.arg1 == mRssiPollToken) {
                         WifiLinkLayerStats stats = getWifiLinkLayerStats();
-                        mWifiDataStall.checkForDataStall(mLastLinkLayerStats, stats);
+                        if (mWifiDataStall.checkForDataStall(mLastLinkLayerStats, stats)) {
+                            mWifiMetrics.addToWifiUsabilityStatsList(WifiUsabilityStats.LABEL_BAD);
+                        }
                         mWifiMetrics.incrementWifiLinkLayerUsageStats(stats);
                         mLastLinkLayerStats = stats;
 
                         // Get Info and continue polling
                         fetchRssiLinkSpeedAndFrequencyNative();
+                        mWifiMetrics.updateWifiUsabilityStatsEntries(mWifiInfo, stats);
                         // Send the update score to network agent.
                         mWifiScoreReport.calculateAndReportScore(
                                 mWifiInfo, mNetworkAgent, mWifiMetrics);
