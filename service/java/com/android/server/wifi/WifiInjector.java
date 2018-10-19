078f3989bdf5a4799a06f504a65957f574c9c7f9
Quang Luong <qal@google.com>
2018-10-18 17:04:07 -0700

    Changed WifiTrafficPoller to use WifiInfo for Tx/Rx pkt count
    Removed WifiTrafficPoller's TrafficHandler mechanism for getting
    the current Tx and Rx packet counts. Instead of calling
    notifyOnDataActivity() every 1 second and polling WifiNative for
    the Tx/Rx packet counts, ClientModeImpl will call into
    WifiTrafficPoller's notifyOnDataActivity() directly on every
    CMD_RSSI_POLL (3 seconds), passing in the Tx/Rx packet counts
    maintained by WifiInfo.
    
    Removed traffic handler mechanism from WifiTrafficPoller since the
    polling behavior is already done in ClientModeImpl.
    
    Removed broadcast receiver checking for network connected and
    screen on from WifiTrafficPoller since CMD_RSSI_POLL is processed
    while connected and with screen on already.
    
    Removed WifiNative and Context argument from WifiTrafficPoller
    constructor.
    
    Changed arguments of notifyOnDataActivity() from (String ifacename)
    to (long txPkts, long rxPkts), and changed visibility to package
    protected so ClientModeImpl can call it directly.
    
    Added WifiTrafficPoller argument to ClientModeImpl constructor to
    have a reference to the WifiTrafficPoller instance.
    
    Changed WifiInjector to pass the created WifiTrafficPoller into the
    ClientModeImpl constructor.
    
    Added call to WifiTrafficPoller.notifyOnDataActivity in ClientModeImpl
    CMD_RSSI_POLL handling.
    
    WifiTrafficPollerTest has been modified to reflect these changes and
    notifyOnDataActivity is called directly instead of trigged by callbacks.
    Testing for screen state has also been removed.
    
    Added test in ClientModeImplTest to check if WifiTrackerPoller is
    called into when CMD_RSSI_POLL is processed.
    
    Updated ClientModeImpl constructor in ClientModeImplTest.
    
    Changed references to mTrafficPoller to mWifiTrafficPoller.
    
    Removed DBG flag from WifiTrafficPoller.
    
    Removed verbose logging mechanism from WifiTrafficPoller.
    
    Bug: 111691443
    Test: wifitest/runtest.sh, adb logcat, visual UI check
    Change-Id: Id3ffc89a865486545be8e332d2eef3e8a0ba571b

--- service/java/com/android/server/wifi/WifiInjector.java
+++ service/java/com/android/server/wifi/WifiInjector.java
@@ -80,7 +80,7 @@ public class WifiInjector {
     private final FrameworkFacade mFrameworkFacade = new FrameworkFacade();
     private final HandlerThread mWifiServiceHandlerThread;
     private final HandlerThread mWifiCoreHandlerThread;
-    private final WifiTrafficPoller mTrafficPoller;
+    private final WifiTrafficPoller mWifiTrafficPoller;
     private final WifiCountryCode mCountryCode;
     private final BackupManagerProxy mBackupManagerProxy = new BackupManagerProxy();
     private final WifiApConfigStore mWifiApConfigStore;
@@ -201,8 +201,7 @@ public class WifiInjector {
         mWifiP2pNative = new WifiP2pNative(mSupplicantP2pIfaceHal, mHalDeviceManager);
 
         // Now get instances of all the objects that depend on the HandlerThreads
-        mTrafficPoller = new WifiTrafficPoller(mContext, mWifiServiceHandlerThread.getLooper(),
-                mWifiNative);
+        mWifiTrafficPoller = new WifiTrafficPoller(mWifiServiceHandlerThread.getLooper());
         mCountryCode = new WifiCountryCode(mWifiNative,
                 SystemProperties.get(BOOT_DEFAULT_WIFI_COUNTRY_CODE),
                 mContext.getResources()
@@ -273,7 +272,7 @@ public class WifiInjector {
                 clientModeImplLooper, UserManager.get(mContext),
                 this, mBackupManagerProxy, mCountryCode, mWifiNative, mWifiScoreCard,
                 new WrongPasswordNotifier(mContext, mFrameworkFacade),
-                mSarManager);
+                mSarManager, mWifiTrafficPoller);
         mActiveModeWarden = new ActiveModeWarden(this, mContext, clientModeImplLooper,
                 mWifiNative, new DefaultModeManager(mContext, clientModeImplLooper),
                 mBatteryStats);
@@ -360,7 +359,7 @@ public class WifiInjector {
     }
 
     public WifiTrafficPoller getWifiTrafficPoller() {
-        return mTrafficPoller;
+        return mWifiTrafficPoller;
     }
 
     public WifiCountryCode getWifiCountryCode() {
