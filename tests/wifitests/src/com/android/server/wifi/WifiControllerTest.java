5264d0988ff53cd23ac2cd96dc6779e7246a04ee
Roger Wang <wangroger@google.com>
2019-01-11 14:54:41 +0800

    WifiController: Add delay for letting wifi module clean reset
    While doing watchdog recovery, framework immediately does
    wifi on after off, it will not fully clean up wifi module.
    This commit help wifi module is reset and clean.
    
    Bug: 121391366
    Test: Manual test
    Test: Unit test
    Change-Id: Ic393a009fc1ae3db4d3f879733905d59fa4b4e25

--- tests/wifitests/src/com/android/server/wifi/WifiControllerTest.java
+++ tests/wifitests/src/com/android/server/wifi/WifiControllerTest.java
@@ -34,6 +34,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.location.LocationManager;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
@@ -43,6 +44,7 @@ import android.util.Log;
 
 import androidx.test.filters.SmallTest;
 
+import com.android.internal.R;
 import com.android.internal.util.IState;
 import com.android.internal.util.StateMachine;
 
@@ -65,6 +67,8 @@ import java.lang.reflect.Method;
 public class WifiControllerTest {
 
     private static final String TAG = "WifiControllerTest";
+
+    private static final int TEST_WIFI_RECOVERY_DELAY_MS = 2000;
 
     private void dumpState() {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -90,6 +94,7 @@ public class WifiControllerTest {
 
     TestLooper mLooper;
     @Mock Context mContext;
+    @Mock Resources mResources;
     @Mock FrameworkFacade mFacade;
     @Mock WifiSettingsStore mSettingsStore;
     @Mock ClientModeImpl mClientModeImpl;
@@ -110,6 +115,11 @@ public class WifiControllerTest {
         mLooper = new TestLooper();
 
         initializeSettingsStore();
+
+        when(mContext.getResources()).thenReturn(mResources);
+
+        when(mResources.getInteger(R.integer.config_wifi_framework_recovery_timeout_delay))
+                .thenReturn(TEST_WIFI_RECOVERY_DELAY_MS);
 
         mWifiController = new WifiController(mContext, mClientModeImpl, mLooper.getLooper(),
                 mSettingsStore, mLooper.getLooper(), mFacade, mActiveModeWarden);
@@ -224,6 +234,7 @@ public class WifiControllerTest {
                 .thenReturn(Settings.Secure.LOCATION_MODE_OFF);
 
         reset(mContext, mActiveModeWarden);
+        when(mContext.getResources()).thenReturn(mResources);
         mWifiController = new WifiController(mContext, mClientModeImpl, mLooper.getLooper(),
                 mSettingsStore, mLooper.getLooper(), mFacade, mActiveModeWarden);
         ArgumentCaptor<BroadcastReceiver> bcastRxCaptor = ArgumentCaptor.forClass(
@@ -258,6 +269,7 @@ public class WifiControllerTest {
                 .thenReturn(Settings.Secure.LOCATION_MODE_HIGH_ACCURACY);
 
         reset(mContext, mActiveModeWarden);
+        when(mContext.getResources()).thenReturn(mResources);
         mWifiController = new WifiController(mContext, mClientModeImpl, mLooper.getLooper(),
                 mSettingsStore, mLooper.getLooper(), mFacade, mActiveModeWarden);
         ArgumentCaptor<BroadcastReceiver> bcastRxCaptor = ArgumentCaptor.forClass(
@@ -1038,6 +1050,8 @@ public class WifiControllerTest {
         reset(mActiveModeWarden);
         mWifiController.sendMessage(CMD_RECOVERY_RESTART_WIFI);
         mLooper.dispatchAll();
+        mLooper.moveTimeForward(TEST_WIFI_RECOVERY_DELAY_MS);
+        mLooper.dispatchAll();
         InOrder inOrder = inOrder(mActiveModeWarden);
         verify(mActiveModeWarden).disableWifi();
         verify(mActiveModeWarden).enterScanOnlyMode();
@@ -1060,6 +1074,9 @@ public class WifiControllerTest {
 
         mWifiController.sendMessage(CMD_RECOVERY_RESTART_WIFI);
         mLooper.dispatchAll();
+        mLooper.moveTimeForward(TEST_WIFI_RECOVERY_DELAY_MS);
+        mLooper.dispatchAll();
+
         InOrder inOrder = inOrder(mActiveModeWarden);
         inOrder.verify(mActiveModeWarden).shutdownWifi();
         inOrder.verify(mActiveModeWarden).enterClientMode();
