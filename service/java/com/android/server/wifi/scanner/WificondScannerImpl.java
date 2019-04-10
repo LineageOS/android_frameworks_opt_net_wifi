450842db540be8586a81c11d51a19a33d0a834d8
Aaron Kling <webgeek1234@gmail.com>
2019-04-10 18:28:55 -0500

    Allow nvwifi to disable wifi scanning
    This is needed to prevent the wifi direct controller from getting
    disconnected
    
    Change-Id: I7b0c3de653ce708bcf5cdbf196fe43de22a9d2d2

--- service/java/com/android/server/wifi/scanner/WificondScannerImpl.java
+++ service/java/com/android/server/wifi/scanner/WificondScannerImpl.java
@@ -30,6 +30,7 @@ import com.android.server.wifi.Clock;
 import com.android.server.wifi.ScanDetail;
 import com.android.server.wifi.WifiMonitor;
 import com.android.server.wifi.WifiNative;
+import com.android.server.wifi.WifiStateMachine;
 import com.android.server.wifi.scanner.ChannelHelper.ChannelCollection;
 import com.android.server.wifi.util.ScanResultUtil;
 
@@ -43,6 +44,8 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.annotation.concurrent.GuardedBy;
+
+import vendor.nvidia.hardware.server.wifi.NvWifi;
 
 /**
  * Implementation of the WifiScanner HAL API that uses wificond to perform all scans
@@ -81,6 +84,9 @@ public class WificondScannerImpl extends WifiScannerImpl implements Handler.Call
     // Settings for the currently running pno scan, null if no scan active
     private LastPnoScanSettings mLastPnoScanSettings = null;
 
+    private NvWifi mNvWifi;
+    private boolean mIsScanAllowed;
+
     private final boolean mHwPnoScanSupported;
 
     /**
@@ -105,6 +111,9 @@ public class WificondScannerImpl extends WifiScannerImpl implements Handler.Call
         mAlarmManager = (AlarmManager) mContext.getSystemService(Context.ALARM_SERVICE);
         mEventHandler = new Handler(looper, this);
         mClock = clock;
+
+        mNvWifi = WifiStateMachine.getNvWifi();
+        mIsScanAllowed = true;
 
         // Check if the device supports HW PNO scans.
         mHwPnoScanSupported = mContext.getResources().getBoolean(
@@ -207,6 +216,9 @@ public class WificondScannerImpl extends WifiScannerImpl implements Handler.Call
 
                 mScanTimeoutListener = new AlarmManager.OnAlarmListener() {
                     @Override public void onAlarm() {
+                        if (mNvWifi != null) {
+                            mIsScanAllowed = mNvWifi.isScanAllowed(-3, null);
+                        }
                         handleScanTimeout();
                     }
                 };
@@ -419,13 +431,20 @@ public class WificondScannerImpl extends WifiScannerImpl implements Handler.Call
         mWifiNative.stopPnoScan(mIfaceName);
     }
 
+    public boolean isBlakeConnected() {
+        if (mNvWifi != null) {
+            return mNvWifi.isBlakeConnected();
+        }
+        return false;
+    }
+
     /**
      * Hw Pno Scan is required only for disconnected PNO when the device supports it.
      * @param isConnectedPno Whether this is connected PNO vs disconnected PNO.
      * @return true if HW PNO scan is required, false otherwise.
      */
     private boolean isHwPnoScanRequired(boolean isConnectedPno) {
-        return (!isConnectedPno && mHwPnoScanSupported);
+        return (!isConnectedPno && mHwPnoScanSupported && !isBlakeConnected());
     }
 
     @Override
