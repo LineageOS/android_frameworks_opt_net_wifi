f61e141c8f344e9e92dfdc4203b7dee0762a8a09
Ecco Park <eccopark@google.com>
2019-04-17 10:53:24 -0700

    PasspointNetworkEvaluator: Ignore networks disconnected by user.
    WifiConfigManager maintains a blacklist of all ephemeral networks that
    the user decided to disconnect. Avoid connecting back to them in all the
    evaluators (already handled in suggestion & scored evaluators).
    
    Bug: 130309348
    Test: ./frameworks/opt/net/wifi/tests/wifitests/runtests.sh
    Change-Id: If72207b166b76bd32ad1bcfcd70fb069464d650f
    Signed-off-by: Ecco Park <eccopark@google.com>

--- service/java/com/android/server/wifi/hotspot2/PasspointNetworkEvaluator.java
+++ service/java/com/android/server/wifi/hotspot2/PasspointNetworkEvaluator.java
@@ -19,6 +19,7 @@ package com.android.server.wifi.hotspot2;
 import static com.android.server.wifi.hotspot2.Utils.isCarrierEapMethod;
 
 import android.annotation.NonNull;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.hotspot2.PasspointConfiguration;
 import android.os.Process;
@@ -131,10 +132,19 @@ public class PasspointNetworkEvaluator implements WifiNetworkSelector.NetworkEva
             if (!scanDetail.getNetworkDetail().isInterworking()) {
                 continue;
             }
+            ScanResult scanResult = scanDetail.getScanResult();
+
+            // If the user previously disconnects this network, don't select it.
+            if (mWifiConfigManager.wasEphemeralNetworkDeleted(
+                    ScanResultUtil.createQuotedSSID(scanResult.SSID))) {
+                mLocalLog.log("Ignoring disabled the SSID of Passpoint AP: "
+                        + WifiNetworkSelector.toScanId(scanResult));
+                continue;
+            }
 
             // Find the best provider for this ScanDetail.
             Pair<PasspointProvider, PasspointMatch> bestProvider =
-                    mPasspointManager.matchProvider(scanDetail.getScanResult());
+                    mPasspointManager.matchProvider(scanResult);
             if (bestProvider != null) {
                 if (bestProvider.first.isSimCredential() && !mWifiConfigManager.isSimPresent()) {
                     // Skip providers backed by SIM credential when SIM is not present.
