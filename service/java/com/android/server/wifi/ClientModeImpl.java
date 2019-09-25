d0925f6d6adb94d11bb1348a388a7069b67ce206
Roshan Pius <rpius@google.com>
2019-09-25 12:10:40 -0700

    WifiServiceImpl: Ignore null binder objects
    Listeners are optional for WifiManager.connect/forget/save methods. So,
    remove the null check. ClientModeImpl handles these correctly.
    
    Bug: 141627840
    Test: Manual connection via settings
    Change-Id: I80ad2df5ef3924741092d3f3c52cdc53ebaa1609

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -6169,9 +6169,9 @@ public class ClientModeImpl extends StateMachine {
     /**
      * Trigger network connection and provide status via the provided callback.
      */
-    public void connect(WifiConfiguration config, int netId, IBinder binder,
+    public void connect(WifiConfiguration config, int netId, @Nullable IBinder binder,
             @Nullable IActionListener callback, int callbackIdentifier, int callingUid) {
-        if (callback != null) {
+        if (callback != null && binder != null) {
             synchronized (mProcessingActionListeners) {
                 mProcessingActionListeners.add(binder, callback, callbackIdentifier);
             }
@@ -6184,9 +6184,9 @@ public class ClientModeImpl extends StateMachine {
     /**
      * Trigger network save and provide status via the provided callback.
      */
-    public void save(WifiConfiguration config, IBinder binder, @Nullable IActionListener callback,
-            int callbackIdentifier, int callingUid) {
-        if (callback != null) {
+    public void save(WifiConfiguration config, @Nullable IBinder binder,
+            @Nullable IActionListener callback, int callbackIdentifier, int callingUid) {
+        if (callback != null && binder != null) {
             synchronized (mProcessingActionListeners) {
                 mProcessingActionListeners.add(binder, callback, callbackIdentifier);
             }
@@ -6199,9 +6199,9 @@ public class ClientModeImpl extends StateMachine {
     /**
      * Trigger network forget and provide status via the provided callback.
      */
-    public void forget(int netId, IBinder binder, @Nullable IActionListener callback,
+    public void forget(int netId, @Nullable IBinder binder, @Nullable IActionListener callback,
             int callbackIdentifier, int callingUid) {
-        if (callback != null) {
+        if (callback != null && binder != null) {
             synchronized (mProcessingActionListeners) {
                 mProcessingActionListeners.add(binder, callback, callbackIdentifier);
             }
--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -3258,10 +3258,6 @@ public class WifiServiceImpl extends BaseWifiService {
     @Override
     public void connect(WifiConfiguration config, int netId, IBinder binder,
             @Nullable IActionListener callback, int callbackIdentifier) {
-        // verify arguments
-        if (binder == null) {
-            throw new IllegalArgumentException("Binder must not be null");
-        }
         if (!isPrivileged(Binder.getCallingPid(), Binder.getCallingUid())) {
             throw new SecurityException(TAG + ": Permission denied");
         }
@@ -3279,10 +3275,6 @@ public class WifiServiceImpl extends BaseWifiService {
     @Override
     public void save(WifiConfiguration config, IBinder binder, @Nullable IActionListener callback,
             int callbackIdentifier) {
-        // verify arguments
-        if (binder == null) {
-            throw new IllegalArgumentException("Binder must not be null");
-        }
         if (!isPrivileged(Binder.getCallingPid(), Binder.getCallingUid())) {
             throw new SecurityException(TAG + ": Permission denied");
         }
@@ -3299,10 +3291,6 @@ public class WifiServiceImpl extends BaseWifiService {
     @Override
     public void forget(int netId, IBinder binder, @Nullable IActionListener callback,
             int callbackIdentifier) {
-        // verify arguments
-        if (binder == null) {
-            throw new IllegalArgumentException("Binder must not be null");
-        }
         if (!isPrivileged(Binder.getCallingPid(), Binder.getCallingUid())) {
             throw new SecurityException(TAG + ": Permission denied");
         }
