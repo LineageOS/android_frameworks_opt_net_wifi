1f7dadee8b4dcc41716599fb8d9fa5eb4e8e35a5
Roshan Pius <rpius@google.com>
2019-10-24 15:21:13 -0700

    WifiStack: Unit tests should use the same jarjar rules
    Bug: 143246705
    Test: atest com.android.server.wifi
    Change-Id: I4d610e5494e6c2ad5ceef8b07b771c3fc48a702b

--- tests/wifitests/src/com/android/server/wifi/hotspot2/ServiceProviderVerifierTest.java
+++ tests/wifitests/src/com/android/server/wifi/hotspot2/ServiceProviderVerifierTest.java
@@ -32,15 +32,15 @@ import android.util.Pair;
 
 import androidx.test.filters.SmallTest;
 
-import com.android.org.bouncycastle.asn1.ASN1EncodableVector;
-import com.android.org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import com.android.org.bouncycastle.asn1.DEROctetString;
-import com.android.org.bouncycastle.asn1.DERSequence;
-import com.android.org.bouncycastle.asn1.DERTaggedObject;
-import com.android.org.bouncycastle.asn1.DERUTF8String;
-import com.android.org.bouncycastle.asn1.x509.GeneralName;
 import com.android.server.wifi.WifiBaseTest;
 
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.DEROctetString;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.DERTaggedObject;
+import org.bouncycastle.asn1.DERUTF8String;
+import org.bouncycastle.asn1.x509.GeneralName;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
