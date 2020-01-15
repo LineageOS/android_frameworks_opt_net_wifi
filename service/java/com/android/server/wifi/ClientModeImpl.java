8096d1a0d71f8f8e6dcf7991aafffe9cef8df956
David Su <dysu@google.com>
2020-01-15 15:19:27 -0800

    Remove redundant call to NetworkInfo.setExtraInfo
    setExtraInfo is @hide, so it shouldn't be called
    by Wifi. It is also redundant, since extraInfo is
    null by default and is not changed by anything
    else, so there is no need to call
    setExtraInfo(null) explicitly.
    
    Bug: 135998869
    Test: atest FrameworksWifiTests
    Change-Id: I7de969b8e6f884ca32c76fb01a142985837f00db

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -2418,7 +2418,6 @@ public class ClientModeImpl extends StateMachine {
         Intent intent = new Intent(WifiManager.NETWORK_STATE_CHANGED_ACTION);
         intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
         NetworkInfo networkInfo = new NetworkInfo(mNetworkInfo);
-        networkInfo.setExtraInfo(null);
         intent.putExtra(WifiManager.EXTRA_NETWORK_INFO, networkInfo);
         //TODO(b/69974497) This should be non-sticky, but settings needs fixing first.
         mContext.sendStickyBroadcastAsUser(intent, UserHandle.ALL);
