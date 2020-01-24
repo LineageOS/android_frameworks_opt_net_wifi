1750ca5df88fb23ed3b44030341f74459b90ebc9
David Su <dysu@google.com>
2020-01-23 18:58:06 -0800

    Rename removeDoubleQuotes to sanitizeSsid
    Bug: 145745815
    Test: compiles
    Change-Id: I2ecb8185a8ea4645043f52b4029ca8b047808e5d

--- libs/WifiTrackerLib/src/com/android/wifitrackerlib/PasspointWifiEntry.java
+++ libs/WifiTrackerLib/src/com/android/wifitrackerlib/PasspointWifiEntry.java
@@ -16,7 +16,7 @@
 
 package com.android.wifitrackerlib;
 
-import static android.net.wifi.WifiInfo.removeDoubleQuotes;
+import static android.net.wifi.WifiInfo.sanitizeSsid;
 
 import static androidx.core.util.Preconditions.checkNotNull;
 
@@ -118,7 +118,7 @@ class PasspointWifiEntry extends WifiEntry {
 
     @Override
     public String getSsid() {
-        return mWifiConfig != null ? removeDoubleQuotes(mWifiConfig.SSID) : null;
+        return mWifiConfig != null ? sanitizeSsid(mWifiConfig.SSID) : null;
     }
 
     @Override
