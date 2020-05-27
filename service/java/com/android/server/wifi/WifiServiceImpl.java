36ed29992b28fe9e313ec2818dae1b8c7d626f32
Ahmed ElArabawy <arabawy@google.com>
2020-05-27 08:11:48 -0700

    Handle failure in softAp start gracefully
    In the current code, if start of a softAp fails for any reason, it can
    no longer start again until device boots. This is because the failure
    handling misses resetting the tethering state.
    This commit handles this by resetting the state on failure.
    
    Bug: 155695448
    Test: atest FrameworksWifiTests
    Test: On a device that does not support 6GHz band
          $ adb shell cmd wifi start-softap kai_softap open -b 6 <==Fail
          $ adb shell cmd wifi start-softap kai_softap open -b 5 <==Succeed
    Change-Id: I6b573d6d7f88ae48a63f20a20f9d4af54a87eba7

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -879,6 +879,14 @@ public class WifiServiceImpl extends BaseWifiService {
 
         mLog.info("startSoftAp uid=%").c(Binder.getCallingUid()).flush();
 
+        SoftApConfiguration softApConfig = null;
+        if (wifiConfig != null) {
+            softApConfig = ApConfigUtil.fromWifiConfiguration(wifiConfig);
+            if (softApConfig == null) {
+                return false;
+            }
+        }
+
         if (!mTetheredSoftApTracker.setEnablingIfAllowed()) {
             mLog.err("Tethering is already active.").flush();
             return false;
@@ -890,17 +898,14 @@ public class WifiServiceImpl extends BaseWifiService {
             mLohsSoftApTracker.stopAll();
         }
 
-        if (wifiConfig != null) {
-            SoftApConfiguration softApConfig = ApConfigUtil.fromWifiConfiguration(wifiConfig);
-            if (softApConfig == null) return false;
-            return startSoftApInternal(new SoftApModeConfiguration(
-                    WifiManager.IFACE_IP_MODE_TETHERED,
-                    softApConfig, mTetheredSoftApTracker.getSoftApCapability()));
-        } else {
-            return startSoftApInternal(new SoftApModeConfiguration(
-                    WifiManager.IFACE_IP_MODE_TETHERED, null,
-                    mTetheredSoftApTracker.getSoftApCapability()));
+        if (!startSoftApInternal(new SoftApModeConfiguration(
+                WifiManager.IFACE_IP_MODE_TETHERED, softApConfig,
+                mTetheredSoftApTracker.getSoftApCapability()))) {
+            mTetheredSoftApTracker.setFailedWhileEnabling();
+            return false;
         }
+
+        return true;
     }
 
     private boolean validateSoftApBand(int apBand) {
@@ -951,11 +956,15 @@ public class WifiServiceImpl extends BaseWifiService {
             mLohsSoftApTracker.stopAll();
         }
 
-        return startSoftApInternal(new SoftApModeConfiguration(
+        if (!startSoftApInternal(new SoftApModeConfiguration(
                 WifiManager.IFACE_IP_MODE_TETHERED, softApConfig,
-                mTetheredSoftApTracker.getSoftApCapability()));
-    }
+                mTetheredSoftApTracker.getSoftApCapability()))) {
+            mTetheredSoftApTracker.setFailedWhileEnabling();
+            return false;
+        }
 
+        return true;
+    }
 
     /**
      * Internal method to start softap mode. Callers of this method should have already checked
@@ -1050,6 +1059,14 @@ public class WifiServiceImpl extends BaseWifiService {
                 }
                 mTetheredSoftApState = WIFI_AP_STATE_ENABLING;
                 return true;
+            }
+        }
+
+        public void setFailedWhileEnabling() {
+            synchronized (mLock) {
+                if (mTetheredSoftApState == WIFI_AP_STATE_ENABLING) {
+                    mTetheredSoftApState = WIFI_AP_STATE_FAILED;
+                }
             }
         }
 
