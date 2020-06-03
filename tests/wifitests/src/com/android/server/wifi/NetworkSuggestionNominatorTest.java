cdd9dbff03f7c7712f68d80f67390a4f03001158
Nate Jiang <qiangjiang@google.com>
2020-06-03 15:14:14 -0700

    [Suggestion] block insecure Enterprise suggestion
    Block adding and connecting to insecure Enterprise suggestion.
    Bug: 157822251
    Test: atest com.android.server.wifi
    
    Change-Id: Ic0741df81a5b50b4e9f98e17d95262946a659118

--- tests/wifitests/src/com/android/server/wifi/NetworkSuggestionNominatorTest.java
+++ tests/wifitests/src/com/android/server/wifi/NetworkSuggestionNominatorTest.java
@@ -163,10 +163,48 @@ public class NetworkSuggestionNominatorTest extends WifiBaseTest {
                     connectableNetworks.add(Pair.create(scanDetail, configuration));
                 });
 
-
         validateConnectableNetworks(connectableNetworks, scanSsids[0]);
-
         verifyAddToWifiConfigManager(suggestions[0].wns.wifiConfiguration);
+    }
+
+    @Test
+    public void testSelectNetworkSuggestionForOneMatchWithInsecureEnterpriseSuggestion() {
+        String[] scanSsids = {"test1"};
+        String[] bssids = {"6c:f3:7f:ae:8c:f3"};
+        int[] freqs = {2470};
+        String[] caps = {"[WPA2-EAP-CCMP][ESS]"};
+        int[] levels = {-67};
+        String[] suggestionSsids = {"\"" + scanSsids[0] + "\""};
+        int[] securities = {SECURITY_EAP};
+        boolean[] appInteractions = {true};
+        boolean[] meteredness = {true};
+        int[] priorities = {-1};
+        int[] uids = {TEST_UID};
+        String[] packageNames = {TEST_PACKAGE};
+        boolean[] autojoin = {true};
+        boolean[] shareWithUser = {true};
+
+        ScanDetail[] scanDetails =
+                buildScanDetails(scanSsids, bssids, freqs, caps, levels, mClock);
+        ExtendedWifiNetworkSuggestion[] suggestions = buildNetworkSuggestions(suggestionSsids,
+                securities, appInteractions, meteredness, priorities, uids,
+                packageNames, autojoin, shareWithUser);
+        WifiConfiguration config = suggestions[0].wns.wifiConfiguration;
+        config.enterpriseConfig.setCaPath(null);
+        // Link the scan result with suggestions.
+        linkScanDetailsWithNetworkSuggestions(scanDetails, suggestions);
+        // setup config manager interactions.
+        setupAddToWifiConfigManager(suggestions[0].wns.wifiConfiguration);
+
+        List<Pair<ScanDetail, WifiConfiguration>> connectableNetworks = new ArrayList<>();
+        mNetworkSuggestionNominator.nominateNetworks(
+                Arrays.asList(scanDetails), null, null, true, false,
+                (ScanDetail scanDetail, WifiConfiguration configuration) -> {
+                    connectableNetworks.add(Pair.create(scanDetail, configuration));
+                });
+
+        // Verify no network is nominated.
+        assertTrue(connectableNetworks.isEmpty());
     }
 
     /**
