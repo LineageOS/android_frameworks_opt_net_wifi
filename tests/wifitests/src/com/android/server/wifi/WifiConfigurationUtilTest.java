18e932aec8672fef4f9fbeb6bb0d19ae20558898
Nate Jiang <qiangjiang@google.com>
2025-02-05 23:02:21 +0000

    Add size check for IP config
    Flag: EXEMPT bugfix
    Bug: 349550024
    Test: atest com.android.server.wifi
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:57e446cfdc95fce61be986d98ffc81e8bb70504a)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:44dc9e6feb930a9c20bd7732ed088acf3b07e53f)
    Merged-In: I761dbb38ffa2e4dcebad2a6abb8d37d45a3b1fd8
    Change-Id: I761dbb38ffa2e4dcebad2a6abb8d37d45a3b1fd8

--- tests/wifitests/src/com/android/server/wifi/WifiConfigurationUtilTest.java
+++ tests/wifitests/src/com/android/server/wifi/WifiConfigurationUtilTest.java
@@ -24,6 +24,7 @@ import static org.junit.Assert.*;
 import android.content.pm.UserInfo;
 import android.net.IpConfiguration;
 import android.net.MacAddress;
+import android.net.ProxyInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiEnterpriseConfig;
 import android.net.wifi.WifiManager;
@@ -1102,5 +1103,28 @@ public class WifiConfigurationUtilTest extends WifiBaseTest {
 
         assertTrue(WifiConfigurationUtil.hasEnterpriseConfigChanged(eapConfig1.enterpriseConfig,
                 eapConfig2.enterpriseConfig));
+    }
+
+    @Test
+    public void testInvalidStaticIpConfig() {
+        WifiConfiguration pskConfig = WifiConfigurationTestUtil.createPskNetwork();
+        IpConfiguration ipConfig =
+                WifiConfigurationTestUtil.createStaticIpConfigurationWithPacProxy();
+        ipConfig.getStaticIpConfiguration().domains = "a".repeat(513);
+        pskConfig.setIpConfiguration(ipConfig);
+        assertFalse(WifiConfigurationUtil.validate(pskConfig, SUPPORTED_FEATURES_ALL,
+                WifiConfigurationUtil.VALIDATE_FOR_ADD));
+    }
+    @Test
+    public void testInvalidProxyInfo() {
+        WifiConfiguration pskConfig = WifiConfigurationTestUtil.createPskNetwork();
+        IpConfiguration ipConfig =
+                WifiConfigurationTestUtil.createStaticIpConfigurationWithStaticProxy();
+        ProxyInfo proxyInfo = ProxyInfo.buildDirectProxy(ipConfig.getHttpProxy().getHost(),
+                ipConfig.getHttpProxy().getPort(), List.of("a".repeat(513)));
+        ipConfig.setHttpProxy(proxyInfo);
+        pskConfig.setIpConfiguration(ipConfig);
+        assertFalse(WifiConfigurationUtil.validate(pskConfig, SUPPORTED_FEATURES_ALL,
+                WifiConfigurationUtil.VALIDATE_FOR_ADD));
     }
 }
